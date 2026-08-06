d9dad1e756eefa40fcff792662a92feb3830d839
Aayan Khan <aayan.khan02072007@gmail.com>
2026-08-06 21:10:47 +0530

    Initial

--- lifehub-backend/src/main/java/com/aayan/lifehub/dto/auth/RegisterResponse.java
+++ lifehub-backend/src/main/java/com/aayan/lifehub/dto/auth/RegisterResponse.java
@@ -10,4 +10,16 @@ public class RegisterResponse {
         this.name = name;
         this.email = email;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
